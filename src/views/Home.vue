836b2de3421b50cd4bc794b29a7f430298c762aa
Chen Gong <gongcchen999@gmail.com>
2021-08-10 16:54:37 -0400

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+
+  <div class='home'>
+    <div v-if="projects.length">
+      
+      <div v-for="project in projects" :key="project.id">
+        <SingleProject :project="project" @delete="handleDelete" @complete="handleComplete"/>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+// @ is an alias to /src
+import SingleProject from '../components/SingleProject.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    SingleProject
+  },
+  data() {
+    return {
+      projects: []
+    }
+  },
+  mounted() {
+    fetch('http://localhost:3000/projects')
+      .then(res => res.json())
+      .then(data => this.projects = data)
+      .catch(err => console.log(err.message))
+  },
+  methods: {
+    handleDelete(id) {
+      this.projects = this.projects.filter((project)=>{
+        return project.id !== id
+      })
+    },
+    handleComplete(id) {
+      this.projects.forEach(project => {
+        if(project.id === id){
+          project.complete = !project.complete
+        }
+      });
+    }
+  }
+}
+</script>
